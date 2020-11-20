daa86e4447e4234096f8aa586f6768c558152f35
Mohit <2018eeb1165@iitrpr.ac.in>
2020-11-20 12:40:22 +0530

    Register app and Add Train and ReleasedTrain models

--- rail/models.py
+++ rail/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Train(models.Model):
+    trainNumber = models.IntegerField(primary_key = True, help_text= "Train number should be of 4-5 characters and should be unique")
+    starts = models.CharField()
+    ends = models.CharField()
+    name = models.CharField()
+
+
+class ReleasedTrain(models.Model):
+    train = models.ForeignKey('Train', on_delete=models.CASCADE, related_query_name='released_trains')
+    departureDate = models.DateField()
+    departureTime = models.TimeField()
+    AcNumber = models.IntegerField()
+    SlNumber = models.IntegerField()
+    releasedDate = models.DateField()
+    releasedTime = models.TimeField()
+
+
