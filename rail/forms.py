94ec9b4550a082f2ce7f8ef32af7c41b91a2fc59
Mohit <2018eeb1165@iitrpr.ac.in>
2020-11-21 12:37:30 +0530

    Changes in add_train form and html

--- rail/forms.py
+++ rail/forms.py
@@ -4,9 +4,9 @@ from .validators import isTrainNumberUnique
 
 class TrainForm(forms.Form):
     trainNumber = forms.IntegerField(help_text= "Train number should be of 4-5 characters and should be unique", widget = forms.NumberInput(), max_value=99999, min_value=1000, validators = [isTrainNumberUnique, ], label = 'Train Number')
-    starts = forms.CharField(max_length = 20, widget = forms.TextInput(), label = 'Source')
-    ends = forms.CharField(max_length = 20, widget = forms.TextInput(), label = 'Destination')
-    name = forms.CharField(max_length = 30, widget = forms.TextInput(), label = 'Name of the Train')
+    starts = forms.CharField(max_length = 20, widget = forms.TextInput(attrs={'placeholder' :"Start Station",  'required': True , 'autofocus' : True}), label = 'Source')
+    ends = forms.CharField(max_length = 20, widget = forms.TextInput(attrs={'placeholder' :"Destination",  'required': True }), label = 'Destination')
+    name = forms.CharField(max_length = 30, widget = forms.TextInput(attrs={'placeholder' :"Train Name", 'required': True }), label = 'Name of the Train')
 
 
     def clean(self):
@@ -15,4 +15,8 @@ class TrainForm(forms.Form):
         ends = self.cleaned_data.get('ends')
         if(starts == ends):
             raise forms.ValidationError('Source and Destination must be different')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=20, widget = forms.TextInput(attrs={'placeholder' :"Email address" 'required' 'autofocus'}), label = "User Name")
 
