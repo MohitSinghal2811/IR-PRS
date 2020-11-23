165f0d404cee7ce2049ddb0c07defbe7b6f9d7f5
Mohit <2018eeb1165@iitrpr.ac.in>
2020-11-23 23:04:41 +0530

    release_train form done

--- rail/my_widgets.py
+++ rail/my_widgets.py
@@ -31,3 +31,18 @@ class MyTimeWidget(forms.Widget):
         context = self.get_context(name, value, attrs)
         template = loader.get_template(self.template_name).render(context)
         return mark_safe(template)
+
+
+class MyTestWidget(forms.Widget):
+    template_name = 'widgets/test_field.html'
+
+    def get_context(self, name, value, attrs=None):
+        return {'widget': {
+            'name': name,
+            'value': value,
+        }}
+
+    def render(self, name, value, attrs=None, renderer = None):
+        context = self.get_context(name, value, attrs)
+        template = loader.get_template(self.template_name).render(context)
+        return mark_safe(template)
